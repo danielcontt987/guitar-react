17bb610230d1f5b3952788d21cd0a217b54c7ee9
Daniel_Contreras98 <jose.contreras@gremti.com>
2025-03-03 19:07:36 -0600

    Functions increase, decrease, romeve, remove all cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,164 +1,75 @@
+// import { useState } from "react"
+import { useState, useEffect } from "react"
+import Guitar from "./components/Guitar"
 import Header from "./components/Header"
+import { db } from "./data/db";
 
 
 function App() {
+  //Definir el state
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function addToCart(item) {
+    const exist = cart.findIndex((i) => i.id === item.id);
+    if (exist >= 0) {
+      const updatedCart = [...cart];
+      updatedCart[exist].quantity += 1;
+      setCart(updatedCart);
+    }else{
+      item.quantity = 1;
+      setCart((cart) => [...cart, item]);
+    }
+  }
+
+  function removeToCart(id) {
+    const updatedCart = cart.filter((item) => item.id !== id);
+    setCart(updatedCart);
+  }
+
+  function increaseToCard(id) {
+    const exist = cart.findIndex((i) => i.id === id);
+    const updatedCart = [...cart];
+    updatedCart[exist].quantity += 1;
+    setCart(updatedCart);
+  }
+
+  function decreaseToCard(id) {
+    const exist = cart.findIndex((i) => i.id === id);
+    const updatedCart = [...cart];
+    updatedCart[exist].quantity -= 1;
+    setCart(updatedCart);
+    if (updatedCart[exist].quantity === 0) {
+       removeToCart(id);
+    }
+  }
+
+  function removeAllCart() {
+    setCart([]);
+  }
+
+ 
   return (
   <>
-      <Header />
+      <Header
+        cart={cart}
+        removeToCart={removeToCart}
+        increaseToCard={increaseToCard}
+        decreaseToCard={decreaseToCard}
+        removeAllCart={removeAllCart}
+      />
       <main className="container-xl mt-5">
           <h2 className="text-center">Nuestra Colección</h2>
 
           <div className="row mt-5">
-              <div className="col-md-6 col-lg-4 my-4 row align-items-center">
-                  <div className="col-4">
-                      <img className="img-fluid" src="./public/img/guitarra_01.jpg" alt="imagen guitarra" />
-                  </div>
-                  <div className="col-8">
-                      <h3 className="text-black fs-4 fw-bold text-uppercase">Lukather</h3>
-                      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
-                          nesciunt, amet deserunt</p>
-                      <p className="fw-black text-primary fs-3">$299</p>
-                      <button type="button" className="btn btn-dark w-100">Agregar al Carrito</button>
-                  </div>
-              </div>
-
-              <div className="col-md-6 col-lg-4 my-4 row align-items-center">
-                  <div className="col-4">
-                      <img className="img-fluid" src="./public/img/guitarra_02.jpg" alt="imagen guitarra" />
-                  </div>
-                  <div className="col-8">
-                      <h3 className="text-black fs-4 fw-bold text-uppercase">SRV</h3>
-                      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
-                          nesciunt, amet deserunt</p>
-                      <p className="fw-black text-primary fs-3">$299</p>
-                      <button type="button" className="btn btn-dark w-100 ">Agregar al Carrito</button>
-                  </div>
-              </div>
-
-              <div className="col-md-6 col-lg-4 my-4 row align-items-center">
-                  <div className="col-4">
-                      <img className="img-fluid" src="./public/img/guitarra_03.jpg" alt="imagen guitarra" />
-                  </div>
-                  <div className="col-8">
-                      <h3 className="text-black fs-4 fw-bold text-uppercase">Borland</h3>
-                      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
-                          nesciunt, amet deserunt</p>
-                      <p className="fw-black text-primary fs-3">$299</p>
-                      <button type="button" className="btn btn-dark w-100 ">Agregar al Carrito</button>
-                  </div>
-              </div>
-
-              <div className="col-md-6 col-lg-4 my-4 row align-items-center">
-                  <div className="col-4">
-                      <img className="img-fluid" src="./public/img/guitarra_04.jpg" alt="imagen guitarra" />
-                  </div>
-                  <div className="col-8">
-                      <h3 className="text-black fs-4 fw-bold text-uppercase">Vai</h3>
-                      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
-                          nesciunt, amet deserunt</p>
-                      <p className="fw-black text-primary fs-3">$299</p>
-                      <button type="button" className="btn btn-dark w-100 ">Agregar al Carrito</button>
-                  </div>
-              </div>
-
-              <div className="col-md-6 col-lg-4 my-4 row align-items-center">
-                  <div className="col-4">
-                      <img className="img-fluid" src="./public/img/guitarra_05.jpg" alt="imagen guitarra" />
-                  </div>
-                  <div className="col-8">
-                      <h3 className="text-black fs-4 fw-bold text-uppercase">Thompson</h3>
-                      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
-                          nesciunt, amet deserunt</p>
-                      <p className="fw-black text-primary fs-3">$299</p>
-                      <button type="button" className="btn btn-dark w-100 ">Agregar al Carrito</button>
-                  </div>
-              </div>
-
-              <div className="col-md-6 col-lg-4 my-4 row align-items-center">
-                  <div className="col-4">
-                      <img className="img-fluid" src="./public/img/guitarra_06.jpg" alt="imagen guitarra" />
-                  </div>
-                  <div className="col-8">
-                      <h3 className="text-black fs-4 fw-bold text-uppercase">White</h3>
-                      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
-                          nesciunt, amet deserunt</p>
-                      <p className="fw-black text-primary fs-3">$299</p>
-                      <button type="button" className="btn btn-dark w-100 ">Agregar al Carrito</button>
-                  </div>
-              </div>
-
-              <div className="col-md-6 col-lg-4 my-4 row align-items-center">
-                  <div className="col-4">
-                      <img className="img-fluid" src="./public/img/guitarra_07.jpg" alt="imagen guitarra" />
-                  </div>
-                  <div className="col-8">
-                      <h3 className="text-black fs-4 fw-bold text-uppercase">Cobain</h3>
-                      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
-                          nesciunt, amet deserunt</p>
-                      <p className="fw-black text-primary fs-3">$299</p>
-                      <button type="button" className="btn btn-dark w-100 ">Agregar al Carrito</button>
-                  </div>
-              </div>
-              <div className="col-md-6 col-lg-4 my-4 row align-items-center">
-                  <div className="col-4">
-                      <img className="img-fluid" src="./public/img/guitarra_08.jpg" alt="imagen guitarra" />
-                  </div>
-                  <div className="col-8">
-                      <h3 className="text-black fs-4 fw-bold text-uppercase">Dale</h3>
-                      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
-                          nesciunt, amet deserunt</p>
-                      <p className="fw-black text-primary fs-3">$299</p>
-                      <button type="button" className="btn btn-dark w-100 ">Agregar al Carrito</button>
-                  </div>
-              </div>
-              <div className="col-md-6 col-lg-4 my-4 row align-items-center">
-                  <div className="col-4">
-                      <img className="img-fluid" src="./public/img/guitarra_09.jpg" alt="imagen guitarra" />
-                  </div>
-                  <div className="col-8">
-                      <h3 className="text-black fs-4 fw-bold text-uppercase">Krieger</h3>
-                      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
-                          nesciunt, amet deserunt</p>
-                      <p className="fw-black text-primary fs-3">$299</p>
-                      <button type="button" className="btn btn-dark w-100 ">Agregar al Carrito</button>
-                  </div>
-              </div>
-              <div className="col-md-6 col-lg-4 my-4 row align-items-center">
-                  <div className="col-4">
-                      <img className="img-fluid" src="./public/img/guitarra_10.jpg" alt="imagen guitarra" />
-                  </div>
-                  <div className="col-8">
-                      <h3 className="text-black fs-4 fw-bold text-uppercase">Campbell</h3>
-                      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
-                          nesciunt, amet deserunt</p>
-                      <p className="fw-black text-primary fs-3">$299</p>
-                      <button type="button" className="btn btn-dark w-100 ">Agregar al Carrito</button>
-                  </div>
-              </div>
-              <div className="col-md-6 col-lg-4 my-4 row align-items-center">
-                  <div className="col-4">
-                      <img className="img-fluid" src="./public/img/guitarra_11.jpg" alt="imagen guitarra" />
-                  </div>
-                  <div className="col-8">
-                      <h3 className="text-black fs-4 fw-bold text-uppercase">Reed</h3>
-                      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
-                          nesciunt, amet deserunt</p>
-                      <p className="fw-black text-primary fs-3">$299</p>
-                      <button type="button" className="btn btn-dark w-100 ">Agregar al Carrito</button>
-                  </div>
-              </div>
-              <div className="col-md-6 col-lg-4 my-4 row align-items-center">
-                  <div className="col-4">
-                      <img className="img-fluid" src="./public/img/guitarra_12.jpg" alt="imagen guitarra" />
-                  </div>
-                  <div className="col-8">
-                      <h3 className="text-black fs-4 fw-bold text-uppercase">Hazel</h3>
-                      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem
-                          nesciunt, amet deserunt</p>
-                      <p className="fw-black text-primary fs-3">$299</p>
-                      <button type="button" className="btn btn-dark w-100 ">Agregar al Carrito</button>
-                  </div>
-              </div>
+            {data.map((guitar) => (
+              <Guitar
+                key={guitar.id} 
+                guitar={guitar}
+                setCart={setCart} 
+                addToCart={addToCart}
+              />
+            ))}
           </div>
       </main>
       
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,11 @@
-export default function Header() {
+import { useMemo } from "react";
+
+export default function Header({cart, removeToCart, increaseToCard, decreaseToCard, removeAllCart}) {
+ 
+  //State derivado
+
+  const total = useMemo(() => cart.reduce((acc, item) => acc + item.price * item.quantity, 0), [cart]);
+  const isEmpty = useMemo(() =>cart.length === 0, [cart]);
   return (
     <header className="py-5 header">
           <div className="container-xl">
@@ -13,47 +20,54 @@ export default function Header() {
                           <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
 
                           <div id="carrito" className="bg-white p-3">
-                              <p className="text-center">El carrito esta vacio</p>
-                              <table className="w-100 table">
-                                  <thead>
-                                      <tr>
-                                          <th>Imagen</th>
-                                          <th>Nombre</th>
-                                          <th>Precio</th>
-                                          <th>Cantidad</th>
-                                          <th></th>
-                                      </tr>
-                                  </thead>
-                                  <tbody>
-                                      <tr>
-                                          <td>
-                                              <img className="img-fluid" src="./public/img/guitarra_02.jpg"
-                                                  alt="imagen guitarra" />
-                                          </td>
-                                          <td>SRV</td>
-                                          <td className="fw-bold">
-                                              $299
-                                          </td>
-                                          <td className="flex align-items-start gap-4">
-                                              <button type="button" className="btn btn-dark">
-                                                  -
-                                              </button>
-                                              1
-                                              <button type="button" className="btn btn-dark">
-                                                  +
-                                              </button>
-                                          </td>
-                                          <td>
-                                              <button className="btn btn-danger" type="button">
-                                                  X
-                                              </button>
-                                          </td>
-                                      </tr>
-                                  </tbody>
-                              </table>
+                                {isEmpty ? (
+                                    <p className="text-center">El carrito esta vacio</p>
+                                ) : (
+                                    <>
+                                    <table className="w-100 table">
+                                        <thead>
+                                            <tr>
+                                                <th>Imagen</th>
+                                                <th>Nombre</th>
+                                                <th>Precio</th>
+                                                <th>Cantidad</th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {cart.map((item) => (
+                                                <tr key={item.id}>
+                                                    <td>
+                                                        <img className="img-fluid" src={`/img/${item.image}.jpg`}
+                                                            alt="imagen guitarra" />
+                                                    </td>
+                                                    <td>{item.name}</td>
+                                                    <td className="fw-bold">
+                                                        ${item.price}
+                                                    </td>
+                                                    <td className="flex align-items-start gap-4">
+                                                        <button type="button" className="btn btn-dark" onClick={() => decreaseToCard(item.id)}>
+                                                            -
+                                                        </button>
+                                                        {item.quantity}
+                                                        <button type="button" className="btn btn-dark" onClick={() => increaseToCard(item.id)}>
+                                                            +
+                                                        </button>
+                                                    </td>
+                                                    <td>
+                                                        <button className="btn btn-danger" type="button" onClick={() => removeToCart(item.id)}>
+                                                            X
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
 
-                              <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
-                              <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+                                    <p className="text-end">Total pagar: <span className="fw-bold">${total}</span></p>
+                                    <button className="btn btn-dark w-100 mt-3 p-2" onClick={() => removeAllCart()}>Vaciar Carrito</button>
+                                    </>  
+                                )}
                           </div>
                       </div>
                   </nav>
